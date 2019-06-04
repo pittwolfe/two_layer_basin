d4e1c8d412247962b10a38e84445dd91f6de1415
Christopher L.P. Wolfe <christopher.wolfe@stonybrook.edu>
2019-06-04 17:44:24 -0400

    Operators and solver now use sparse matrices

--- two_layer_basin.py
+++ two_layer_basin.py
@@ -5,6 +5,8 @@
 import numpy as np
 import scipy as sp
 import numpy.linalg
+import scipy.sparse
+from scipy.sparse import kron, identity, diags
 
 from .chebfun import *
 
@@ -25,6 +27,9 @@ class two_layer_model(object):
         else:
             self.Ny = Ny
 
+        self.boundary_conditions_initialized = False
+        self.operators_initialized = False
+
         self.init_grid()
 
 
@@ -43,52 +48,58 @@ class two_layer_model(object):
         self.boundary_mask[: ,0 ] = 1
         self.boundary_mask[: ,-1] = 1
 
-        self.f = np.diag(1 + self.b*self.yy_flat)
+        self.f = diags(1 + self.b*self.y)
 
 
     def init_forcing(self, K, θ):
         if isinstance(K, numpy.ndarray):
-            self.K = np.diag(K.flatten())
+            self.K = diags(K.flatten())
         else:
-            self.K = np.diag(np.repeat(K, self.N))
+            self.K = diags(np.repeat(K, self.N))
 
         self.θ = θ
 
 
-    def init_operators(self):
+    def init_operators(self, reset=False):
+        if self.operators_initialized and not reset:
+            return
+
         dx2 = self.dx@self.dx
         dy2 = self.dy@self.dy
 
-        Ix = np.identity(self.Nx+1)
-        Iy = np.identity(self.Ny+1)
+        self.Ix = identity(self.Nx+1)
+        self.Iy = identity(self.Ny+1)
 
-        self.Dx = np.kron(Iy, self.dx)
-        self.Dy = np.kron(self.dy, Ix)
-        self.D2x = np.kron(Iy, dx2)
-        self.D2y = np.kron(dy2, Ix)
+        self.Dx = kron(self.Iy, self.dx, format='csr')
+        self.Dy = kron(self.dy, self.Ix, format='csr')
+        self.D2x = kron(self.Iy, dx2, format='csr')
+        self.D2y = kron(dy2, self.Ix, format='csr')
         self.Δ = self.D2x + self.D2y
 
-        self.I = np.identity(self.N)
+        self.I = identity(self.N)
 
-        self.visc = self.Ek*self.Δ
+        self.visc = (self.Ek*self.Δ).tolil()
 
-        self.ηy = -self.Dy - self.K@self.f@self.Dx
-        self.ηx = +self.Dx - self.K@self.f@self.Dy
+        self.ηy = (-self.Dy - self.K@kron(self.f,         self.dx)).tolil()
+        self.ηx = ( self.Dx - self.K@kron(self.f@self.dy, self.Ix)).tolil()
 
-        self.fu = self.f.copy()
-        self.fv = self.f.copy()
+        self.fu = kron(self.f, self.Ix, format='lil')
+        self.fv = self.fu.copy()
 
-        self.Dxu = self.Dx.copy()
-        self.Dyv = self.Dy.copy()
-        self.rIη = -self.r*self.I
+        self.Dxu = self.Dx.tolil(copy=True)
+        self.Dyv = self.Dy.tolil(copy=True)
+        self.rIη = (-self.r*self.I).tolil(copy=True)
 
+        self.operators_initialized = True
 
-    def init_boundary_conditions(self):
+    def init_boundary_conditions(self, reset=False):
         # setup boundary conditions
+
+        if self.boundary_conditions_initialized and not reset:
+            return
 
         idx_bc = self.boundary_mask.flatten() == 1
 
-        # apply the boundary conditions to the sub-blocks
         self.visc[idx_bc,:] = 0
         self.fu[idx_bc,:] = 0
         self.fu[idx_bc,idx_bc] = 1
@@ -134,46 +145,47 @@ class two_layer_model(object):
         self.rhs = np.zeros(3*self.N)
         self.rhs[2*self.N:] = -self.r*θrhs
 
+        self.boundary_conditions_initialized = True
 
-    def solve(self, init_operators=True, init_boundary_conditions=True):
-        if init_operators:
-            self.init_operators()
+    def solve(self):
+        self.init_operators()
+        self.init_boundary_conditions()
 
-        if init_boundary_conditions:
-            self.init_boundary_conditions()
-
-        L = np.block([
+        L = sp.sparse.bmat([
             [self.fu,   -self.visc,   self.ηy],
             [self.visc,  self.fv,     self.ηx],
             [self.Dxu,   self.Dyv,    self.rIη]
-        ])
+        ], format='csr')
 
-        sol = sp.linalg.solve(L, self.rhs.flatten())
+        # hack to get around 32 bit memory limit in umfpack
+        L.indices = L.indices.astype(np.int64)
+        L.indptr  = L.indptr.astype(np.int64)
+        sol = sp.sparse.linalg.spsolve(L, self.rhs.flatten(), use_umfpack=True)
         self.u = np.reshape(sol[:self.N],         (self.Ny+1, self.Nx+1))
         self.v = np.reshape(sol[self.N:2*self.N], (self.Ny+1, self.Nx+1))
         self.η = np.reshape(sol[2*self.N:],       (self.Ny+1, self.Nx+1))
 
+
     def calc_derived_quantities(self):
-        self.diags2d = {}
         self.ustar = apply_operator(self.K@self.Dx, self.η)
         self.vstar = apply_operator(self.K@self.Dy, self.η)
-
         self.ustar[self.boundary_mask == 1] = 0
         self.vstar[self.boundary_mask == 1] = 0
+
         self.ubar = self.u - self.ustar
         self.vbar = self.v - self.vstar
 
+
         self.viscu = apply_operator(self.Ek*self.Δ, self.u)
         self.viscv = apply_operator(self.Ek*self.Δ, self.v)
-        self.fv = apply_operator(self.f, self.v)
-        self.fu = apply_operator(self.f, self.u)
+        self.fv = apply_operator(kron(self.f, self.Ix), self.v)
+        self.fu = apply_operator(kron(self.f, self.Ix), self.u)
         self.ηx = apply_operator(self.Dx, self.η)
         self.ηy = apply_operator(self.Dy, self.η)
-        self.fKηx = apply_operator(self.K@self.f@self.Dx, self.η)
-        self.fKηy = apply_operator(self.K@self.f@self.Dy, self.η)
+        self.fKηx = apply_operator(self.K@kron(self.f, self.dx), self.η)
+        self.fKηy = apply_operator(self.K@kron(self.f*self.dy, self.Ix), self.η)
         self.fKηx[self.boundary_mask == 1] = 0
         self.fKηy[self.boundary_mask == 1] = 0
-
 
         self.ux = apply_operator(self.Dx, self.u)
         self.vy = apply_operator(self.Dy, self.v)
@@ -218,34 +230,49 @@ class two_layer_model(object):
         return interpn((self.y, self.x), fld,  (yyi, xxi), method='splinef2d')
 
 
-    def calc_interpolated_quantities(self, NXi, NYi=None):
+    def interp_grid_1d(self, NXi, NYi=None):
         if NYi is None:
             NYi = int(self.Ly*NXi/self.Lx)
 
-        self.xi = np.linspace(0, self.Lx, NXi)
-        self.yi = np.linspace(-self.Ly/2, self.Ly/2, NYi)
+        xi = np.linspace(0, self.Lx, NXi)
+        yi = np.linspace(-self.Ly/2, self.Ly/2, NYi)
 
-        xxi, yyi = np.meshgrid(self.xi, self.yi)
+        return xi, yi
 
-        self.ηi     = self.interp_field2d(xxi, yyi, self.η)
-        self.ui     = self.interp_field2d(xxi, yyi, self.u)
-        self.vi     = self.interp_field2d(xxi, yyi, self.v)
-        self.wi     = self.interp_field2d(xxi, yyi, self.w)
-        self.ubari  = self.interp_field2d(xxi, yyi, self.ubar)
-        self.vbari  = self.interp_field2d(xxi, yyi, self.vbar)
-        self.wbari  = self.interp_field2d(xxi, yyi, self.wbar)
-        self.ustari = self.interp_field2d(xxi, yyi, self.ustar)
-        self.vstari = self.interp_field2d(xxi, yyi, self.vstar)
+    def interp_grid_2d(self, NXi, NYi=None):
+        xi, yi = self.interp_grid_1d(NXi, NYi=None)
 
-        self.w_yinti = self.interp_field1d(self.x, self.xi, self.w_yint)
-        self.wbar_yinti = self.interp_field1d(self.x, self.xi, self.wbar_yint)
-        self.rzoci = self.interp_field1d(self.x, self.xi, self.rzoc)
-        self.mzoci = self.interp_field1d(self.x, self.xi, self.mzoc)
+        return np.meshgrid(xi, yi)
 
-        self.w_xinti = self.interp_field1d(self.y, self.yi, self.w_xint)
-        self.wbar_xinti = self.interp_field1d(self.y, self.yi, self.wbar_xint)
-        self.rmoci = self.interp_field1d(self.y, self.yi, self.rmoc)
-        self.mmoci = self.interp_field1d(self.y, self.yi, self.mmoc)
 
-        self.xxi = xxi
-        self.yyi = yyi
+#     def calc_interpolated_quantities(self, NXi, NYi=None):
+#         if NYi is None:
+#             NYi = int(self.Ly*NXi/self.Lx)
+#
+#         self.xi = np.linspace(0, self.Lx, NXi)
+#         self.yi = np.linspace(-self.Ly/2, self.Ly/2, NYi)
+#
+#         xxi, yyi = np.meshgrid(self.xi, self.yi)
+#
+#         self.ηi     = self.interp_field2d(xxi, yyi, self.η)
+#         self.ui     = self.interp_field2d(xxi, yyi, self.u)
+#         self.vi     = self.interp_field2d(xxi, yyi, self.v)
+#         self.wi     = self.interp_field2d(xxi, yyi, self.w)
+#         self.ubari  = self.interp_field2d(xxi, yyi, self.ubar)
+#         self.vbari  = self.interp_field2d(xxi, yyi, self.vbar)
+#         self.wbari  = self.interp_field2d(xxi, yyi, self.wbar)
+#         self.ustari = self.interp_field2d(xxi, yyi, self.ustar)
+#         self.vstari = self.interp_field2d(xxi, yyi, self.vstar)
+#
+#         self.w_yinti = self.interp_field1d(self.x, self.xi, self.w_yint)
+#         self.wbar_yinti = self.interp_field1d(self.x, self.xi, self.wbar_yint)
+#         self.rzoci = self.interp_field1d(self.x, self.xi, self.rzoc)
+#         self.mzoci = self.interp_field1d(self.x, self.xi, self.mzoc)
+#
+#         self.w_xinti = self.interp_field1d(self.y, self.yi, self.w_xint)
+#         self.wbar_xinti = self.interp_field1d(self.y, self.yi, self.wbar_xint)
+#         self.rmoci = self.interp_field1d(self.y, self.yi, self.rmoc)
+#         self.mmoci = self.interp_field1d(self.y, self.yi, self.mmoc)
+#
+#         self.xxi = xxi
+#         self.yyi = yyi
