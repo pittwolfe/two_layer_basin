68669a2db8a7dced7489654635e883ae19f9c223
Christopher L.P. Wolfe <christopher.wolfe@stonybrook.edu>
2019-06-11 10:16:32 -0400

    Added __init__.py

---
+++ __init__.py
@@ -0,0 +1,17 @@
+"""
+spectral module
+
+Summary
+-------
+Chebeshev spectral models.
+
+Contains
+--------
+chebfun : Generic routines for manipulating Chebyshev series.
+two_layer_basin : Model of two-layer buoyancy-driven circulation in a rectangular basin.
+"""
+
+# For relative imports to work in Python 3.6
+# import os, sys; sys.path.append(os.path.dirname(os.path.realpath(__file__)))
+
+__author__ = 'cwolfe'
